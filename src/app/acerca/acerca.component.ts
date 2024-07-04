e5555642568b0cc5ddc8a38ff6dde232517fba1d
Agustin <agustinlabattaglia92@gmail.com>
2024-07-03 22:45:49 -0300

    Terminada seccion acerca

--- src/app/acerca/acerca.component.ts
+++ src/app/acerca/acerca.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ItemDataService } from '../item-data.service';
+import { Item } from '../lista-item/Item';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-acerca',
   templateUrl: './acerca.component.html',
   styleUrl: './acerca.component.scss'
 })
 export class AcercaComponent {
+  items$ : Observable<Item []>;
+  
+  constructor(private itemDataService : ItemDataService){
+    this.items$ = itemDataService.getOferta();
+  }
 
+  ngOnInit(){
+    
+  }
 }
--- src/app/item-data.service.ts
+++ src/app/item-data.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { Observable, filter, map, tap } from 'rxjs';
 import { Item } from './lista-item/Item';
 const URL= 'https://6684656756e7503d1ae02b13.mockapi.io/poke/item';
 @Injectable({
@@ -14,5 +14,9 @@ export class ItemDataService {
   getAll():Observable<Item[]>{
    return this.http.get<Item[]>(URL)
     .pipe(tap((items:Item[])=>items.forEach(item=>item.cantidad = 0)));
+  }
+  getOferta():Observable<Item[]>{
+    return this.http.get<Item[]>(URL)
+    .pipe(map(data=> data.filter(item=>item.oferta ==true)));
   }
 }
